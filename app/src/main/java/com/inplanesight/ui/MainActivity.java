0ef420d494effe5a3d0cfc383af2ba6e4fcf7e90
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2022-10-05 12:44:43 -0700

    Add starting fragments, other placeholder files

--- app/src/main/java/com/inplanesight/ui/MainActivity.java
+++ app/src/main/java/com/inplanesight/ui/MainActivity.java
@@ -1,16 +1,21 @@
 package com.inplanesight.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.inplanesight.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostController navHostController = new NavHostController(this);
     }
 }

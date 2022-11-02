b9a0f5b00f62088ea1cfc61d7c3ecfa6e63b2ef7
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2022-11-02 14:04:26 -0700

    Hide bottom navs on some fragments

--- app/src/main/java/com/inplanesight/ui/MainActivity.java
+++ app/src/main/java/com/inplanesight/ui/MainActivity.java
@@ -1,8 +1,11 @@
 package com.inplanesight.ui;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.NavHostController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -11,8 +14,11 @@ import androidx.navigation.ui.NavigationUI;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.common.util.ArrayUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.inplanesight.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,20 +27,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            FragmentManager supportFragmentManager = getSupportFragmentManager();
-            NavHostFragment navHostFragment = (NavHostFragment) supportFragmentManager.findFragmentById(R.id.mainNavHost);
-            NavController navController = navHostFragment.getNavController();
-            BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-            NavigationUI.setupWithNavController(bottomNav, navController);
-        } catch (Exception e) {
-            Log.d("test", e.getMessage());
-        }
-
+        setupNav();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    private void setupNav() {
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment) supportFragmentManager.findFragmentById(R.id.mainNavHost);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+
+        navController.addOnDestinationChangedListener((navController1, navDestination, bundle) -> {
+            int[] navlessFragments = { R.id.findAirportFragment2, R.id.myLocationFragment2, R.id. searchFragment2, R.id.checkInFragment, R.id.overviewFragment };
+
+            if (ArrayUtils.contains(navlessFragments, navDestination.getId())) {
+                bottomNav.setVisibility(View.GONE);
+            } else {
+                bottomNav.setVisibility(View.VISIBLE);
+            }
+        });
+
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
